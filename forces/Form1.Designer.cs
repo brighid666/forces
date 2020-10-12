73fee966d6a118e77158a0b135c8d882a1dcdb39
Brighid Ball <rc0040208@student.riversidecollege.ac.uk>
2020-10-12 14:20:41 +0100

    hjhj

--- forces/Form1.Designer.cs
+++ forces/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(44, 34);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(236, 116);
             this.button1.TabIndex = 0;
